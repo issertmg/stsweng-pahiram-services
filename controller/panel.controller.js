cc6f347fc4491132b0f541f0a48cd8c8a55cbd90
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2020-12-01 20:24:38 +0800

    added set locker status validation

--- controller/panel.controller.js
+++ controller/panel.controller.js
@@ -134,7 +134,7 @@ exports.panel_update = async function (req, res) {
             let lockerIndex = req.body.lockernumber - panel.lowerRange;
             let lockerid = panel.lockers[lockerIndex]._id;
             let editable = await isLockerVacantBroken(lockerid);
-            if (editable) {
+            if (editable && isSetStatusValid(req.body.status)) {
                 await Locker.findByIdAndUpdate(lockerid, {status: req.body.status});
             } else {
                 console.log('Locker status cannot be updated.');
@@ -221,6 +221,10 @@ exports.valid_locker_range_get = async function(req, res) {
     console.log(valid);
     if (valid) res.send("valid");
     else res.send("invalid");
+}
+
+function isSetStatusValid(status) {
+    return status === 'vacant' || status === 'broken';
 }
 
 async function isLockerVacantBroken(lockerid) {
