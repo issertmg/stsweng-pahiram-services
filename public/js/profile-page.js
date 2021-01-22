fe1242748da9e9ac56f86a3993867f35f5f58421
issertmg <issertmg@gmail.com>
2021-01-22 18:35:50 +0800

    added on modal show function

--- public/js/profile-page.js
+++ public/js/profile-page.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('#phone').keyup(function () {
-        var value = validator.trim($('#phone').val());
-        var validLength = validator.isLength(value, {min: 10, max: 10});
-        var validNumeral = validator.isNumeric(value, {no_symbols: true});
+        let value = validator.trim($('#phone').val());
+        let validLength = validator.isLength(value, {min: 10, max: 10});
+        let validNumeral = validator.isNumeric(value, {no_symbols: true});
 
         if (!validLength || !validNumeral)
             $('#phoneError').text('Invalid phone number');
@@ -14,8 +14,16 @@ $(document).ready(function () {
                 } else {
                     $('#phoneError').text('Number already taken');
                     $('#submitBtn').prop('disabled', true);
-                } 
+                }
             });
         }
     });
+});
+
+$('#editProfileModal').on('show.bs.modal', function (event) {
+    let button = $(event.relatedTarget);
+    let phone = button.data('phone');
+    $('#phone').val(phone);
+    $('#phoneError').text(' ');
+    $('#submitBtn').prop('disabled', false);
 });
