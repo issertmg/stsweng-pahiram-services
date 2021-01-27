a5f404c228b8491f40a825db9c28240d08b5c7e2
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2021-01-27 11:32:38 +0800

    Update reserve-book.js

--- public/js/reserve-book.js
+++ public/js/reserve-book.js
@@ -124,8 +124,10 @@ $('#borrowBookModal').on('show.bs.modal', (event) => {
 $("#borrowBookSubmit").on("click", function () {
     if (!$("#checkTerms").prop('checked'))
         alert("Please check the box if you agree to the terms and conditions.");
-    else
+    else {
+        $('#submitLockerForm').off("click");
         $("#borrowBookSubmit").trigger("submit");
+    }
 });
 
 $(document).ajaxStart(function () {
